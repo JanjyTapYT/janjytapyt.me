a1015c22f5792cdff7f4ad9ee8b99a24f74ab8ca
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2022-05-09 06:37:59 +0200

    Update Config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: 'https://www.janjytapyt.ml',
+    base: 'https://www.janjytapyt.live',
     contactHook: 'https://discord.com/api/webhooks/963148742918295672/7qeLnALIOiJ4jRwqh6Jyv_w7Oj8HRdSR8skH3VlqHT6u2-yU8fuS9g07UIXSDOMARUbA',
     newsHook: 'https://discord.com/api/webhooks/966084368256872498/rqJCzc0QRITLyg0ZkuMIPhBXGy2_3brWiFxM6yzjON5p8J6T05HFYOCAuxKcmEM14em4',
     meta: {
@@ -9,7 +9,7 @@ module.exports = {
         themeColor: '#ffffff',
         image: 'https://cdn.discordapp.com/attachments/925001268450177065/962412339137761350/unknown.png'
     },
-    titleSuffix: 'janjytapyt.ml',
+    titleSuffix: 'janjytapyt.live',
     titles: {
         '/': 'Home',
         '/projects': 'Projects',
